256b9681f1e0961054c163e2c0189d794f767b81
Gabriel Santos <82888506+GabrielSantosCruz@users.noreply.github.com>
2021-09-27 10:43:04 -0300

    Added a funtion of menu

--- main.py
+++ main.py
@@ -10,44 +10,16 @@ do código, e estou ciente que estes trechos não serão considerados para fins 
 O código e sua evolução pode ser encontrado em: https://github.com/GabrielSantosCruz/alphagens
 ******************************************************************************************/'''
 from biblioteca_de_funcoes import *
-print('''
-░█████╗░██╗░░░░░██████╗░██╗░░██╗░█████╗░░██████╗░███████╗███╗░░██╗░██████╗
-██╔══██╗██║░░░░░██╔══██╗██║░░██║██╔══██╗██╔════╝░██╔════╝████╗░██║██╔════╝
-███████║██║░░░░░██████╔╝███████║███████║██║░░██╗░█████╗░░██╔██╗██║╚█████╗░
-██╔══██║██║░░░░░██╔═══╝░██╔══██║██╔══██║██║░░╚██╗██╔══╝░░██║╚████║░╚═══██╗
-██║░░██║███████╗██║░░░░░██║░░██║██║░░██║╚██████╔╝███████╗██║░╚███║██████╔╝
-╚═╝░░╚═╝╚══════╝╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚══╝╚═════╝░
-==========================================================================
-  #     ==     *    -      @       __    #       **     -_     %     $   %      
-      *     -    *    |--------------------------|  #  -    *   --      @ 
-   *        #         |     1 - Jogar            |      *       -    #   
-      @       *       |     2 - Tutorial         | @     -   #      ==   *
-          #       *   |     3 - Sair             |     #   -  *    %    -
-    @     -    *   %  |--------------------------|   %     -    @      #    
-''')
+menu()
 
 m = check_number_matriz(input('Digite o tamanho da matriz: '))
 n = check_number_matriz(input('Digite a quantidade de cores: '))
 p = build_matriz(m, n)
 
 while True:
-    print_matriz(p)
     print('='*50)
-    break_gens(p, m)
-    print_matriz(p)
-    atual_m = check_int(input("Digite a linha atual: "))
-    atual_n = check_int(input("Digite a coluna atual: "))
-    final_m = check_int(input("Digite a linha final: "))
-    final_n = check_int(input("Digite a coluna final: "))
-    print(f'iten selecionado: {p[atual_m][atual_n]}') # consigo printar o iten que eu quero
-
-    x = p[atual_m][atual_n]
-    y = p[final_m][final_n]
-    p[atual_m][atual_n] = y
-    p[final_m][final_n] = x
+    p = permutation(p)
     print('='*50)
     print_matriz(p)
     print("Apos juntar 3: ")
-    break_gens(p, m)
-        
     print_matriz(p)
